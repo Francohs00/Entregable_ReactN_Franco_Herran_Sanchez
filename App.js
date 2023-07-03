ee1338483ed48900e01219847c8d8cdc4c345f6c
Franco Herran Sanchez <francohs00@gmail.com>
2023-07-02 23:41:55 -0300

    update2-7

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, StatusBar, Platform, SafeAreaView, FlatList, Modal, TouchableOpacity } from 'react-native';
+import {styles} from './styles'
+import { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [borderColor, setBordercolor] = useState('#C5C9E7')
+  const [tasks, setTasks] = useState([])
+  const [isVisible, setIsVisible] = useState(false)
+  const [selectedTask, setSelectedTask] = useState(null)
+
+  const onHandlerFocus = () => {
+    setBordercolor('#424D9E')
+  }
+
+  const onHandlerBlur = () => {
+    setBordercolor('#C5C9E7')
+  }
+
+  const onHandlerChangetext = (Text) => {
+    setTask(Text)
+  }
+  
+  const onHandlercreateTask = () => {
+    console.log("onHandlerCreateTask");
+    console.warn({ task });
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now().toString(),
+        value: task
+      }
+    ])
+
+    setTask('')
+  }
+  const onHandlerModal = (item) => {
+    setIsVisible(true)
+    setSelectedTask(item)
+  }
+
+  const onHandleDelete = (id) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    setIsVisible(false);
+  };
+
+const renderItem = ({ item }) => (
+  <TouchableOpacity onPress={() => onHandlerModal(item.id)} style={styles.containerItem}>
+  <Text style={styles.listItem}>{item.value}</Text>
+  <Text style={styles.icon}>x</Text>
+  </TouchableOpacity>
+)
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput 
+          style={styles.input} 
+          placeholder='agregar item'
+          autoCapitalize='none'
+          autoCorrect={false}
+          cursorColor='#424D9E'
+          selectionColor='#D4D7ED'
+          placeholderTextColor='#C5C9E7'
+          onFocus={onHandlerFocus}
+          onBlur={onHandlerBlur} 
+          onChangeText={onHandlerChangetext}
+          value={task}
+           />
+          <Button disabled={task.length===0} title='Crear' color='blue' onPress={onHandlercreateTask} />
+        </View>
+        
+          <FlatList
+            data={tasks}
+            renderItem={renderItem}
+            contentContainerStyle={styles.list}
+            style={styles.list}
+            alwaysBounceVertical={false}
+            keyExtractor={item => item.id}
+          />
+        
+      </View>
+      <Modal visible={isVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Detalle</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>Estas seguto de querer borrar el item?</Text>
+            <Text>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button
+              title='cancelar'
+              color='blue'
+              onPress={() => setIsVisible(false)}
+              style={styles.Button}
+              />
+            
+            <Button 
+            title="Eliminar" 
+            color="red" 
+            onPress={() => onHandleDelete(selectedTask?.id)
+            } />
+            
+
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,72 @@
+import { StyleSheet, StatusBar } from "react-native";
+
+export const styles = StyleSheet.create({
+    safeArea:{
+      flex:1
+    },
+    
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      marginHorizontal: 20,
+      paddingTop: Platform.OS == 'ios' ? 50 : StatusBar.currentHeight,
+    },
+    
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    
+    input: {
+      flex: 0.95,
+      borderWidth: 1,
+      borderRadius: 10,
+      paddingHorizontal: 10,
+      color: '#000',
+      borderColor: '#000',
+      fontSize: 15,
+    },
+    
+    listContainer: {
+        marginTop: 25,
+        gap:15,
+    },
+    
+    containerItem: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        backgroundColor: '#D4D7ED',
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        color: '#000',
+        shadowColor: "#000000",
+        shadowOffset: {
+          width: 0,
+          height: 7,
+        },
+        shadowOpacity:  0.21,
+        shadowRadius: 7.68,
+        elevation: 10,
+        
+    },
+    
+    listItem: {
+        fontSize: 15,
+        fontWeight: 'bold',
+
+    },
+
+    list: {
+        gap: 15,
+        paddingBottom: 20,
+
+    },
+
+    icon: {
+        color: 'red',
+        fontSize: 18,
+    }
+  });
+  
